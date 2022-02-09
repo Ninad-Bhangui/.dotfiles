d325992af3dabca06d3c2dd9d2fa435b22439f2a
Ninad Bhangui <b.ninad50@gmail.com>
2022-02-09 13:52:49 +0530

    modified

--- nvim/.config/nvim/lua/nmb/lsp.lua
+++ nvim/.config/nvim/lua/nmb/lsp.lua
@@ -2,6 +2,41 @@ local cmp = require('cmp')
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
 local nvim_lsp = require('lspconfig')
 local lspkind = require("lspkind")
+--
+-- lspconfig
+--
+local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    
+    -- Mappings
+    local opts = {noremap=true, silent=true }
+
+    buf_set_keymap('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n','gr','<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n','K','<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n','<leader>dl','<cmd>Telescope diagnostics<CR>', opts)
+    buf_set_keymap('n','<leader>dj','<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n','<leader>dk','<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n','<leader>r','<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts) 
+    buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+-- nvim-cmp supports lsp capabilities
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- Loop to setup multiple servers
+servers = {'tsserver','pyright','gopls','rust_analyzer'}
+for _, lsp in ipairs(servers) do
+    nvim_lsp[lsp].setup {
+        capabilities=capabilities,
+        on_attach = on_attach
+    }
+end
+--
+-- Nvim-cmp setup
+--
+
 cmp.setup({
     snippet = {
       -- REQUIRED - you must specify a snippet engine
@@ -51,11 +86,4 @@ cmp.setup({
       { name = 'cmdline' }
     })
   })
-servers = {'tsserver','pyright','gopls','rust_analyzer'}
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup {
-        capabilities=capabilities        
-    }
-end
+
